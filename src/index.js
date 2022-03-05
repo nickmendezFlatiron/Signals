d80f4851b7bdf677ae4decd67443688b142b8956
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-03-05 14:07:58 -0500

    added searchbar function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-import * as bootstrap from 'bootstrap';
+
+const searchbar = document.getElementById('searchbar')
+const searchMatches = document.getElementById('search-matches')
+
+
+//Alpha Vantage search results and auto-complete best matches list
+// const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchbar.value}&apikey=VXY7DLJ0XAQQON66`;
+
+function marketSearch(url)  {
+  fetch(url)
+    .then(res => res.json())
+    .then(searchresults => {
+      // console.log(searchresults)
+      let bestMatchesArray = (Object.values(searchresults))[0]
+      bestMatchesArray.forEach(bestMatch => search)
+    })
+}
+
+searchbar.addEventListener('change',e => {
+  e.preventDefault()
+  const searchbarValue = e.target.value 
+  let url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchbarValue}&apikey=VXY7DLJ0XAQQON66`
+  marketSearch(url)
+})
