7951855bce6b3e37c10f4dfaa7af300e408a0e71
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-03-05 15:50:23 -0500

    simplified search results

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
-
+//GLOBAL VARIABLES
 const searchbar = document.getElementById('searchbar')
 const searchMatches = document.getElementById('search-matches')
+let matchResults = [] ;
 
 
-//Alpha Vantage search results and auto-complete best matches list
-// const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchbar.value}&apikey=VXY7DLJ0XAQQON66`;
-
-function marketSearch(url)  {
-  fetch(url)
-    .then(res => res.json())
-    .then(searchresults => {
-      // console.log(searchresults)
-      let bestMatchesArray = (Object.values(searchresults))[0]
-      bestMatchesArray.forEach(bestMatch => search)
-    })
-}
+//EVENT LISTENERS
 
 searchbar.addEventListener('change',e => {
   e.preventDefault()
@@ -22,3 +12,36 @@ searchbar.addEventListener('change',e => {
   let url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchbarValue}&apikey=VXY7DLJ0XAQQON66`
   marketSearch(url)
 })
+
+
+
+//FUNCTIONS
+
+//Alpha Vantage API search Results Best Matches
+function marketSearch(url)  {
+  fetch(url)
+    .then(res => res.json())
+    .then(searchresults => {
+      let bestMatchesArray = (Object.values(searchresults))[0]
+      matchResults = []
+      bestMatchesArray.map(bestMatch => simpleResults(bestMatch))
+      console.log(matchResults[0].ticker)
+    })
+}
+
+//this function pushes a simplified object to the matchResults array for every best match found
+function simpleResults(bestMatch) {
+  const bestMatchSimplified = Object.values(bestMatch)
+   console.log(bestMatchSimplified)
+  const stockSymbol = bestMatchSimplified[0].toString()
+  const stockName = bestMatchSimplified[1].toString()
+  const stockType = bestMatchSimplified[2].toString()
+  let match = {
+    name : stockName ,
+    ticker : stockSymbol , 
+    type : stockType 
+  }
+  matchResults.push(match)
+}
+
+
