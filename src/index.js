a01ce2a4cedf0e653cec6f75cec162aaf47c11fc
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-03-05 20:56:47 -0500

    simplified search results

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 //GLOBAL VARIABLES
 const searchbar = document.getElementById('searchbar')
 const searchMatches = document.getElementById('search-matches')
+const searchBtn = document.querySelector('#search-btn')
 let matchResults = [] ;
 
 
 //EVENT LISTENERS
 
-searchbar.addEventListener('change',e => {
+searchBtn.addEventListener('click',e => {
   e.preventDefault()
-  const searchbarValue = e.target.value 
+  const searchbarValue = searchbar.value 
   let url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchbarValue}&apikey=VXY7DLJ0XAQQON66`
   marketSearch(url)
 })
@@ -25,7 +26,8 @@ function marketSearch(url)  {
       let bestMatchesArray = (Object.values(searchresults))[0]
       matchResults = []
       bestMatchesArray.map(bestMatch => simpleResults(bestMatch))
-      console.log(matchResults[0].ticker)
+      // console.log(matchResults[0].ticker)
+      appendResults(matchResults)
     })
 }
 
@@ -36,12 +38,37 @@ function simpleResults(bestMatch) {
   const stockSymbol = bestMatchSimplified[0].toString()
   const stockName = bestMatchSimplified[1].toString()
   const stockType = bestMatchSimplified[2].toString()
+  const stockRegion = bestMatchSimplified[3].toString()
   let match = {
     name : stockName ,
     ticker : stockSymbol , 
-    type : stockType 
+    type : stockType ,
+    region : stockRegion
   }
   matchResults.push(match)
 }
 
+//appends the top 5 search results to the DOM 
+function appendResults(matchResults) {
+  searchMatches.innerHTML = ``
+  let n  = 0
+  matchResults.length < 5 ? n = matchResults.length : n = 5 ;
+  for(let i = 0 ; i < n ; i++)
+   {
+    let match = matchResults[i]
+    let li = document.createElement('li')
+    
+    li.classList.add('list-group-item')
+    li.innerHTML = `${match.name}<span class="text-primary "> (${match.ticker})</span>
+    `
+    searchMatches.appendChild(li)
+    li.addEventListener('click', e => appendDisplay(e))
+  }
+}
 
+//Populates the main info section with the selected 
+function appendDisplay(e) {
+  console.log('I was clicked')
+  console.log (e.target.innerText)
+
+}
